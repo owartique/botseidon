cc8cac2c60ccfc51d4aaa671174a411b02d7a029
owartique <owartique@hotmail.com>
2020-12-03 17:19:15 +0100

    Update MyARM_Pipelined.py

--- minibot/Python/MyARM_Pipelined.py
+++ minibot/Python/MyARM_Pipelined.py
@@ -17,15 +17,19 @@ sleep(0.1)
 GPIO.output(MyARM_ResetPin, GPIO.LOW)
 sleep(0.1)
 
+# set the baud rate
 ToSPI = [0x80, 0x00, 0x4F, 0x00, 0x00]
 FromSPI = MySPI_FPGA.xfer2(ToSPI)
 
+# write the value 5 in the register at address 1
 ToSPI = [0x81, 0x00, 0x00, 0x00, 0x05]
 FromSPI = MySPI_FPGA.xfer2(ToSPI)
 
+# write the value 4 in the register at address 2
 ToSPI = [0x82, 0x00, 0x00, 0x00, 0x04]
 FromSPI = MySPI_FPGA.xfer2(ToSPI)
 
+# 
 GPIO.output(MyARM_ResetPin, GPIO.HIGH)
 sleep(0.1)
 GPIO.output(MyARM_ResetPin, GPIO.LOW)
